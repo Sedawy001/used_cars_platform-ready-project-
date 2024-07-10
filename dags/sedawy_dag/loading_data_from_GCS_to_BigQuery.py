fe9a45f0c509f0f8cd09844fdc19442b2a9531c9
Muhamed Sedawy <muhamedsedawy502@gmail.com>
2024-07-10 19:27:20 +0300

    load data

--- dags/sedawy_dag/loading_data_from_GCS_to_BigQuery.py
+++ dags/sedawy_dag/loading_data_from_GCS_to_BigQuery.py
@@ -20,19 +20,8 @@ dag = DAG(
 
 
 start_task = EmptyOperator(task_id="start_task", dag=dag)
-load_task = GoogleCloudStorageToBigQueryOperator(
-        task_id='load_to_bigquery',
-        bucket='cars-com_dataset.csv',
-        source_objects=['data*.csv'],
-        destination_project_dataset_table='ready-data-de24.Cars_02',
-        source_format='CSV',
-        autodetect=True,
-        field_delimiter=',',
-        write_disposition='Write_append',
-        create_disposition='CREATE_IF_NEEDED',
-        dag=dag,
-    )
+
 
 end_task = EmptyOperator(task_id="end_task", dag=dag)
 
-start_task >> load_task >> end_task
+start_task >>end_task
