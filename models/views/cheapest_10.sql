7532b245bf1f494ae98378b38e9df4adaffb0936
muhamed sedawy <muhamedsedawy502@gmail.com>
2024-09-06 21:33:14 +0000

    commit

---
+++ models/views/cheapest_10.sql
@@ -0,0 +1,9 @@
+select brand , m.model_name , price  
+from {{ source('dbt_msedawy_DMs', 'dim_model') }} m
+join {{ source('dbt_msedawy_DMs', 'fact_car') }} f 
+on m.model_id = f.model_id
+join {{ source('dbt_msedawy_DMs', 'dim_engine') }} e
+on e.engine_id = f.engine_id
+where fuel_type = 'Gasoline' and year >=  2019 and mileage < 50000
+order by price asc
+limit 10
